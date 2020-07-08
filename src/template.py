75d3609091fc7043283198214c61170346dc6f38
dcp <dcp@dcps-MacBook-Air-3.local>
2020-07-07 21:46:04 -0400

    Initial commit of src/ & added .gitignore.

---
+++ src/template.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# pythontemplate.py
+#
+#
+"""
+pythontemplate.py is a template that includes OptionParser-style arguments.
+"""
+
+__author__ = "David C. Petty"
+__copyright__ = "Copyright 2020, David C. Petty"
+__license__ = "https://creativecommons.org/licenses/by-nc-sa/4.0/"
+__version__ = "0.1.0"
+__maintainer__ = "David C. Petty"
+__email__ = "david_petty@psbma.org"
+__status__ = "Hack"
+
+import argparse, os, sys
+
+
+class OptionParser(argparse.ArgumentParser):
+    """Create OptionParser to parse command-line options."""
+    def __init__(self, **kargs):
+        argparse.ArgumentParser.__init__(self, **kargs)
+        # self.remove_argument("-h")
+        self.add_argument("-?", "--help", action="help",
+                          help="show this help message and exit")
+        self.add_argument('--version', action='version',
+                          version=f"%(prog)s {__version__}")
+
+    def error(self, msg):
+        sys.stderr.write("%s: error: %s\n\n" % (self.prog, msg, ))
+        self.print_help()
+        sys.exit(2)
+
+
+def main(argv):
+    """This is a template that includes OptionParser-style arguments."""
+    description = """This is a template that includes OptionParser-style 
+    arguments"""
+    formatter = lambda prog: \
+        argparse.ArgumentDefaultsHelpFormatter(prog, max_help_position=30)
+    parser = OptionParser(description=description, add_help=False,
+                          formatter_class=formatter)
+    arguments = [
+        # c1, c2, action, dest, default, help
+        ('-a', '--arg', 'store', 'ARG', None, 'argument', ),
+        ('-v', '--verbose', 'store_true', 'VERBOSE', False, 'echo status information', ),
+    ]
+    # Add optional arguments with values.
+    for c1, c2, a, v, d, h in arguments:
+        parser.add_argument(c1, c2, action=a, dest=v, default=d, help=h,)
+    # Add positional arguments. REQUIRED is both the string and the variable.
+    parser.add_argument("REQUIRED", help="required argument")
+    # Add positional arguments. OPTIONAL is both the list and the variable.
+    parser.add_argument("OPTIONAL", nargs="*", help="optional arguments")
+    # Parse arguments.
+    pa = parser.parse_args(args=argv[1: ])
+    if pa.ARG:
+        os.environ['ARG'] = pa.ARG
+        print(f"ARG = {pa.ARG}")
+    if pa.REQUIRED:
+        print(f"REQ = {pa.REQUIRED}")
+    if pa.OPTIONAL:
+        print(f"OPT = {pa.OPTIONAL}")
+
+
+if __name__ == '__main__':
+    is_idle, is_pycharm, is_jupyter = (
+        'idlelib' in sys.modules,
+        int(os.getenv('PYCHARM', 0)),
+        '__file__' not in globals()
+    )
+    if any((is_idle, is_pycharm, is_jupyter,)):
+        main(['parsereport.py', 'REQ', 'OPT1', 'OPT2', '-a', 'FOO', ])
+    else:
+        main(sys.argv)
